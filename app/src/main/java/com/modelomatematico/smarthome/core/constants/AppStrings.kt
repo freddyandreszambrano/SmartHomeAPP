7085c6ebc321fb7cbd19ded06fa45e4df3fcf4d6
Freddy Zambrano <freddyfazq0614@gmail.com>
2025-06-07 18:38:44 -0500

    feat(auth): :sparkles: add string constants and custom notification functionality

---
+++ app/src/main/java/com/modelomatematico/smarthome/core/constants/AppStrings.kt
@@ -0,0 +1,10 @@
+package com.modelomatematico.smarthome.core.constants
+
+
+object AppStrings {
+    const val ERROR = "Error"
+    const val INFO = "Informacion"
+    const val SUCCESS = "Exito"
+    const val WARNING = "Advertencia"
+
+}
