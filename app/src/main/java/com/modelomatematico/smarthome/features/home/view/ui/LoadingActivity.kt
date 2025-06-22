9f353d3e67dc5814a10f30460e96b90445edb95a
Freddy Zambrano <freddyfazq0614@gmail.com>
2025-06-21 23:15:52 -0500

    style(home): :lipstick: GridSpacingItemDecoration for RecyclerView and update UI components in HomeActivity

--- app/src/main/java/com/modelomatematico/smarthome/features/home/view/ui/LoadingActivity.kt
+++ app/src/main/java/com/modelomatematico/smarthome/features/home/view/ui/LoadingActivity.kt
@@ -3,6 +3,7 @@ package com.modelomatematico.smarthome.features.home.view.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.modelomatematico.smarthome.features.home.view.components.LoadingScreen
 
 class LoadingActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
